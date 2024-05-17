d523974b758e6f8760d46a3211f9b30395bc48e7
hanzalahwaheed <hanzalah.w@gmail.com>
2024-05-17 22:58:05 +0530

    First Commit

---
+++ backend/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+end
