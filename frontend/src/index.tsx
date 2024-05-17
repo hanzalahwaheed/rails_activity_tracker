13650ad1585b475ffc299357e21d049b76664f60
hanzalahwaheed <hanzalah.w@gmail.com>
2024-05-18 03:49:42 +0530

    add extensions webpacks to fe

---
+++ frontend/src/index.tsx
@@ -0,0 +1,27 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = document.createElement("div");
+root.className = "container";
+document.body.appendChild(root);
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
